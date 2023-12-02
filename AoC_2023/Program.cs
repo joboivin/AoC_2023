5e37e52a0b8ebdc87ede542f6e0abd1539fefae5
joboivin <jobr_guitare@hotmail.com>
2023-12-01 20:02:37 -0500

    Initial structure

---
+++ AoC_2023/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AoC_2023;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ AoC_2023_Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AoC_2023_Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
